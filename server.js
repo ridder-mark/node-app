634e13e8ec14725dd88097d6ebafc00ace923679
Rohan Verma <rohanverma@Rohans-MacBook-Pro-2.local>
2017-10-17 00:11:08 +0530

    test

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 
 //include libraries
-
 const express = require('express');
-var app = express();
-
 const hbs = require('hbs');
 const fs = require('fs');
 
 //initialize port
 const port = process.env.PORT || 3000;
+
+// class variable
+var app = express();
+
+//initialize partials
+hbs.registerPartials(__dirname + '/views/partials');
 
 // initialize web renderer
 app.set('view engine', 'hbs');
@@ -29,8 +32,35 @@ app.use((req, res, next) => {
 
 });
 
+// setup serving static
+app.use(express.static(__dirname + '/public'));
 
+// hbs helpers
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
 
+// page renders
+app.get('/', (req, res) => {
+    res.render('page.hbs', {
+        pageTitle: 'Home Page',
+        pageContent: 'This is the homepage'
+    })
+})
+
+app.get('/project', (req, res) => {
+    res.render('page.hbs', {
+        pageTitle: 'Project Page',
+        pageContent: 'This is a project page',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('page.hbs', {
+        pageTitle: 'About Page',
+        pageContent: 'This is the about page',
+    })
+})
 
 //port status report
 app.listen(port, () => {
